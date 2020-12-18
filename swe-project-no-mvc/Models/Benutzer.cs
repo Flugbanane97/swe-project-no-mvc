e8d31451480cfbab9177aba67fda919983c7326f
danielbluemlein <daniel.bluemlein97@gmail.com>
2020-12-18 11:43:01 +0100

    Benutzer Klasse hinzugefügt

---
+++ swe-project-no-mvc/Models/Benutzer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace swe_project_no_mvc.Models
+{
+    public class Benutzer
+    {
+        public int Id { get; set; }
+        public string Vorname { get; set; }
+        public string Nachanme { get; set; }
+        public string Email { get; set; }
+        public bool Positiv { get; set; }
+        public DateTime PositivAblauf { get; set; }
+
+        public Benutzer()
+        {
+        }
+
+        public Benutzer(int id, string vorname, string nachanme, string email, bool positiv, DateTime positivAblauf)
+        {
+            Id = id;
+            Vorname = vorname;
+            Nachanme = nachanme;
+            Email = email;
+            Positiv = positiv;
+            PositivAblauf = positivAblauf;
+        }
+    }
+}
